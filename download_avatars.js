b9a85cd635ed0f076ba37a115ded10efd7c6b5ea
EdwardXyWang <edwardxywang@gmail.com>
2017-01-17 03:19:00 +0000

    get the json file

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,18 @@
 var request = require('request');
-
+// Use the request module
+var GITHUB_USER = "EdwardXyWang";
+var GITHUB_TOKEN = "99d0794799453d96f02d40a551f3b97650f7cf8a";
+// store token as globle environment variable, call <$gitapil> to show
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // the main function to fetch the contributors avatar
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN +
+  '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
+}
+
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   console.log("Errors:", err);
+//   console.log("Result:", result);
+// });
