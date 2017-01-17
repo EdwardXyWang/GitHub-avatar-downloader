990936eaa4c3479f866c486c07510b88e97d733f
EdwardXyWang <edwardxywang@gmail.com>
2017-01-17 05:37:17 +0000

    realize the terminal input

--- download_avatars.js
+++ download_avatars.js
@@ -4,6 +4,9 @@ var fs = require('fs');
 var GITHUB_USER = "EdwardXyWang";
 var GITHUB_TOKEN = "99d0794799453d96f02d40a551f3b97650f7cf8a";
 // store token as globle environment variable, call <$gitapil> to show
+var repoOwner = process.argv[2];
+var repoName = process.argv[3];
+// get repo owner and repo name
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -23,10 +26,12 @@ function printImage(err, response, body){
   body = JSON.parse(body);
   var urlFile = [];
   var pathFile = [];
+  // Store urlFile and pathFile
   for (var i = 0; i < body.length; i++) {
     urlFile[i] = body[i]['avatar_url'];
     pathFile[i] = './avatars/' + body[i]['login'] + '.jpg'
   };
+  // download them
   for (var i = 0; i < body.length; i++) {
     downloadImageByURL(urlFile[i], pathFile[i]);
   };
@@ -38,10 +43,4 @@ function downloadImageByURL(url, filePath) {
     .pipe(fs.createWriteStream(filePath));
 }
 
-getRepoContributors("jquery", "jquery", printImage);
-
-
-
-
-
-
+getRepoContributors(repoOwner, repoName, printImage);
