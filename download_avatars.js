02a7c2675846c2b1594f13d10f9ce4851a5b1c0e
xwa130 <xwa130@sfu.ca>
2017-03-15 05:05:41 +0000

    separate concern: wrote a download avatars helper

--- download_avatars.js
+++ download_avatars.js
@@ -1,78 +1,15 @@
-const request = require('request');
-const fs = require('fs');
-require('dotenv').config();
+const avatarHelper = require('./download_avatars_helper.js');
 
 const repoOwner = process.argv[2];
 const repoName = process.argv[3];
 
-const GITHUB_USER = process.env.GITHUB_USER;
-const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 // set the warning when the input arguments are not legal
 if (process.argv.length !== 4) {
   throw new Error('Plesse input the right format: repoOwner repoName');
 }
 
-console.log('Welcome to the GitHub Avatar Downloader!');
-
-function getRepoContributors(repoOwner, repoName, cb) {
-  // the main function to fetch the contributors avatar
-  const requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN +
-  '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  // set options
-  var requestOptions = {
-    url: requestURL,
-    headers: {'user-agent': 'GitHub Avatar Downloader - Student Project'}
-  };
-
-  request(requestOptions, cb);
-}
-
-function printImage(err, response, body){
-  console.log('It starts downloading...');
-  if (err) { throw(err); }
-
-  body = JSON.parse(body);
-  // handle error if repo is not found
-  if (body.message === 'Not Found') {
-    throw new Error('Your Repo is not found!');
-  }
-  // handle error if credential is not valid
-  if (body.message === 'Bad credentials') {
-    throw new Error('Credential is not valid!');
-  }
-
-  const [urlFile, pathFile] = makePath(body);
-
-  // download them
-  for (let i = 0; i < body.length; i++) {
-    downloadImageByURL(urlFile[i], pathFile[i]);
-  }
-  console.log("Congratulations! It's done!");
-}
-
-// Store urlFile and the saving path
-function makePath(body) {
-  let urlFile = [];
-  let pathFile = [];
-  // if folder ./avatars does not exist, create one
-  if (!fs.existsSync('./avatars')) {
-    fs.mkdirSync('./avatars');
-  }
-  for (let i = 0; i < body.length; i++) {
-    urlFile[i] = body[i].avatar_url;
-    pathFile[i] = './avatars/' + body[i].login + '.jpg';
-  }
-  return [urlFile, pathFile];
-}
-
-// store the images
-function downloadImageByURL(url, filePath) {
-  request.get(url).on('error', function(err) {throw(err);})
-    .pipe(fs.createWriteStream(filePath));
-}
-
 try {
-  getRepoContributors(repoOwner, repoName, printImage);
+  avatarHelper.getRepoContributors(repoOwner, repoName, avatarHelper.printImage);
 } catch (err) {
   console.log('Error: ', err);
 }
